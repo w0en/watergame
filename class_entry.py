c959da563bdad54d60648d2b882a23aaf82bb78c
owentheman <imsmartypants@gmail.com>
2022-06-12 18:33:33 +0800

    Initial commit for the Entry class in the Water Game. An Entry consists of a color and a property of being hidden, to reflect the values where not all colors are reflected at once. A collection of Entry(s) make up a Tube.

---
+++ class_entry.py
@@ -0,0 +1,35 @@
+class Entry:
+    def __init__(self, color, hidden):
+        self.color = color #hexstring, value of -1 if empty
+        self.hidden = hidden #boolean
+        
+    def __repr__(self):
+        if self.hidden:
+            return "?"
+        else:
+            return f"{self.color}"
+
+    def __str__(self):
+        if self.hidden:
+            return "?"
+        else:
+            return f"{self.color}"
+    
+    def __eq__(self, other):
+        """Simple check if two Entrys are equal. They are equal if neither is empty and they have the same color"""
+        if self.hidden or other.hidden == True:
+            return False
+        else:
+            if self.color == other.color:
+                return True
+            return False
+        
+        
+    def __hash__(self):
+        if self.hidden:
+            return hash(self.color)
+        else:
+            return hash(self.color + 16777216)
+        
+    def unhide(self):
+        self.hidden = False
